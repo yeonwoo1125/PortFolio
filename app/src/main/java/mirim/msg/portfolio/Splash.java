c7167c3b3a90f1638309bc39e720d69a404ced40
c04nh <w2038@e-mirim.hs.kr>
2021-12-25 21:24:01 +0900

    Splash

---
+++ app/src/main/java/mirim/msg/portfolio/Splash.java
@@ -0,0 +1,24 @@
+package mirim.msg.portfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },2000);
+    }
+}
