f2767318a7e7419f419bd4987e369ace1f35c089
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2021-12-29 20:06:05 +0900

    연우_프로필 초안

--- app/src/main/java/mirim/msg/portfolio/MainActivity.java
+++ app/src/main/java/mirim/msg/portfolio/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Intent intent = new Intent(this, Splash.class);
         startActivity(intent);
