12905df524127d781348af1b228700b4b88fd204
c04nh <w2038@e-mirim.hs.kr>
2022-01-02 21:24:43 +0900

    프로필에 toolbar 추가

--- app/src/main/java/mirim/msg/portfolio/nahyunProfile.java
+++ app/src/main/java/mirim/msg/portfolio/nahyunProfile.java
@@ -1,8 +1,14 @@
 package mirim.msg.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class nahyunProfile extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class nahyunProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_nahyun);
+
+
+        //툴바 설정
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.home_btn); //icon 변경
+
     }
+
+
+    //액션 버튼 클릭 이벤트
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
