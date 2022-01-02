ef4ef0ba724fc13007098c9e60829e28485251c7
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-01-02 16:41:57 +0900

    액션바 설정

---
+++ app/src/main/java/mirim/msg/portfolio/yeonwooProject.java
@@ -0,0 +1,15 @@
+package mirim.msg.portfolio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class yeonwooProject extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project_yeonwoo);
+
+
+    }
+}
