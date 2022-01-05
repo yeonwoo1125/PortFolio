344f11a9e20daa18842e5c5bfbf706f881d47f0f
juwan-25 <s2109@e-mirim.hs.kr>
2022-01-05 21:02:03 +0900

    juwan 자바파일

---
+++ app/src/main/java/mirim/msg/portfolio/juwanProfile.java
@@ -0,0 +1,15 @@
+package mirim.msg.portfolio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class juwanProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_juwan);
+
+
+    }
+}
