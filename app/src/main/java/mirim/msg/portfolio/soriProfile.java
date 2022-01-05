75fb949336487635b42eeb1f6ec5b4d5c44992ce
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-01-05 21:06:22 +0900

    profile

--- app/src/main/java/mirim/msg/portfolio/soriProfile.java
+++ app/src/main/java/mirim/msg/portfolio/soriProfile.java
@@ -10,5 +10,6 @@ public class soriProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_sori);
+
     }
 }
