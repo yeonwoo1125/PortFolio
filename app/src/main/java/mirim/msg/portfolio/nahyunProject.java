22700ed238e563ac1fcc2ee0845e37e75096c411
c04nh <w2038@e-mirim.hs.kr>
2022-01-10 13:10:29 +0900

    포트폴리오 버튼 연결

--- app/src/main/java/mirim/msg/portfolio/nahyunProject.java
+++ app/src/main/java/mirim/msg/portfolio/nahyunProject.java
@@ -3,39 +3,21 @@ package mirim.msg.portfolio;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
-public class nahyunProfile<btnListener> extends AppCompatActivity {
-    Button btn;
+public class nahyunProject extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_nahyun);
-
-
+        setContentView(R.layout.project_nahyun);
         //툴바 설정
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기
         //getSupportActionBar().setHomeAsUpIndicator(R.drawable.home_btn); //icon 변경
 
-        btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), nahyunProject.class);
-                startActivity(intent);
-            }
-        });
     }
 
 
@@ -50,5 +32,4 @@ public class nahyunProfile<btnListener> extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
