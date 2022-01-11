3d0d7aeef5775e352c5bb80504f828683ce973d5
c04nh <w2038@e-mirim.hs.kr>
2022-01-11 15:07:53 +0900

    ViewFlipper 추가

--- app/src/main/java/mirim/msg/portfolio/nahyunProject.java
+++ app/src/main/java/mirim/msg/portfolio/nahyunProject.java
@@ -5,9 +5,13 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class nahyunProject extends AppCompatActivity {
-
+    Button btn1, btn2, btn3;
+    ViewFlipper viewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +22,33 @@ public class nahyunProject extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기
         //getSupportActionBar().setHomeAsUpIndicator(R.drawable.home_btn); //icon 변경
 
+        viewFlipper = findViewById(R.id.viewflipper);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(btnListener);
+        btn2.setOnClickListener(btnListener);
+        btn3.setOnClickListener(btnListener);
+
     }
 
-
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn1:
+                   viewFlipper.setDisplayedChild(0);
+                   break;
+                case R.id.btn2:
+                    viewFlipper.setDisplayedChild(1);
+                    break;
+                case R.id.btn3:
+                    viewFlipper.setDisplayedChild(2);
+                    break;
+            }
+        }
+    };
     //액션 버튼 클릭 이벤트
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
